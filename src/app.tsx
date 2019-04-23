d8031050dad20d2e7fbf64df4b5d988a66146839
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-04-23 10:05:51 +0800

    install ant design

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,25 @@
-import { hot } from 'react-hot-loader/root';
+import { Button, DatePicker, Form, Input, Layout } from 'antd';
 import * as React from 'react';
+import { hot } from 'react-hot-loader/root';
 
 function App() {
   const [name, setName] = React.useState('');
 
   return (
     <main>
-      <h1>Simple App</h1>
-      <input value={name} onChange={ev => setName(ev.target.value)} />
+      <Layout>
+        <Form title="Simple Form" layout="vertical">
+          <Form.Item label="Name" required>
+            <Input value={name} onChange={ev => setName(ev.target.value)} />
+          </Form.Item>
+          <Form.Item label="Date">
+            <DatePicker />
+          </Form.Item>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form>
+      </Layout>
     </main>
   );
 }
