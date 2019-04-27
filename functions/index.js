65b15863ed0d363ef4100be46574475c71f49e39
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-04-28 02:30:22 +0800

    send proper html content

--- functions/index.js
+++ functions/index.js
@@ -33,26 +33,73 @@ exports.onNewForm = functions.database
         await sendEmail({
           email: val.email,
           subject: 'Loan application in L',
-          content: `
-            You've applied a loan with the following details:
-            Name: ${val.name}
-            IC/Passport: ${val.id}
-            Addresses: 
-            ${val.addressLine1}
-            ${val.postalCode} ${val.city}, ${val.state}
-            Mobile: ${val.mobileNumber}
-            Email: ${val.email || 'Not Provided'}
-            Company: ${val.company}
-            companyHrNumber: ${val.companyHrNumber}
-            Current Salary: RM ${val.salary}
-            Car Price: RM ${val.carPrice}
-            Down Payment: ${val.downPayment}
-            Tenure: ${val.tenure} years
-            `,
-          // icImage: ${val.icImage}
-          // licenseImage: ${val.licenseImage}
-          // salarySlipFor3MonthsImages: string[];
-          // savingStatementsFor3MonthsImages: string[];
+          content: `<p>You've applied a loan with the following details:</p>
+          <table>
+            <tr>
+              <td>Name:</td><td>${val.name}</td>
+            </tr>
+            <tr>
+              <td>IC/Passport:</td><td>${val.id}</td>
+            </tr>
+            <tr>
+              <td>Addresses:</td><td>${val.addressLine1}<br />
+              ${val.postalCode} ${val.city}, ${val.state}</td>
+            </tr>
+            <tr>
+              <td>Mobile:</td><td>${val.mobileNumber}</td>
+            </tr>
+            <tr>
+              <td>Email:</td><td>${val.email || 'Not Provided'}</td>
+            </tr>
+            <tr>
+              <td>Company:</td><td> ${val.company}</td>
+            </tr>
+            <tr>
+              <td>companyHrNumber:</td><td> ${val.companyHrNumber}</td>
+            </tr>
+            <tr>
+              <td>Current Salary:</td><td> RM ${val.salary}</td>
+            </tr>
+            <tr>
+              <td>Car Price:</td><td> RM ${val.carPrice}</td>
+            </tr>
+            <tr>
+              <td>Down Payment:</td><td> ${val.downPayment}</td>
+            </tr>
+            <tr>
+              <td>Tenure:</td><td> ${val.tenure} years</td>
+            </tr>
+            <tr>
+              <td>IC Image:</td><td> ${
+                val.icImage ? `<img src="${val.icImage}" />` : 'Not Provided'
+              }</td>
+            </tr>
+            <tr>
+              <td>License Image:</td><td> ${
+                val.licenseImage
+                  ? `<img src="${val.licenseImage}" />`
+                  : 'Not Provided'
+              }</td>
+            </tr>
+            ${
+              Array.isArray(val.salarySlipFor3MonthsImages) &&
+              val.salarySlipFor3MonthsImages.length > 0
+                ? `<tr><td colspan="2">Pay Slips</td></tr>
+                <tr><td colspan="2">${val.salarySlipFor3MonthsImages
+                  .map(imgSrc => `<img src="${imgSrc}" />`)
+                  .join('')}</td></tr>`
+                : ''
+            }
+            ${
+              Array.isArray(val.savingStatementsFor3MonthsImages) &&
+              val.savingStatementsFor3MonthsImages.length > 0
+                ? `<tr><td colspan="2">Saving Account Statements</td></tr>
+                <tr><td colspan="2">${val.savingStatementsFor3MonthsImages
+                  .map(imgSrc => `<img src="${imgSrc}" />`)
+                  .join('')}</td></tr>`
+                : ''
+            }
+          </table>`,
           sender: 'noreply@team36.firebaseapp.com'
         });
       } catch (e) {
