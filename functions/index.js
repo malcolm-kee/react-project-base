483538a54f1e7fcda990ece621a83b87a808dc1a
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-04-27 14:24:37 +0800

    send email via gmail

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,36 @@
-const functions = require('firebase-functions');
+'use strict';
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const functions = require('firebase-functions');
+const nodemailer = require('nodemailer');
+
+const gmailEmail = functions.config().gmail.email;
+const gmailPassword = functions.config().gmail.password;
+const mailTransport = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: gmailEmail,
+    pass: gmailPassword
+  }
+});
+
 exports.sendSubmissionEmail = functions.database
   .ref('/forms/{formId}')
-  .onCreate(
-    change =>
-      new Promise((fulfill, reject) => {
-        const val = change.val();
-        console.log('in sendSubmissionEmail handler');
-        console.log(val);
-        fulfill();
-      })
-  );
+  .onCreate(async function handleNewFormSubmission(change) {
+    const val = change.val();
+    console.log('in sendSubmissionEmail handler');
+    console.log(val);
+
+    if (val && val.email) {
+      try {
+        await mailTransport.sendEmail({
+          from: '"Smart Loan" <noreply@smartyloan.firebaseapp.com>',
+          to: val.email,
+          subject: 'Car Loan Submission Received',
+          text: 'Thanks!'
+        });
+      } catch (e) {
+        console.error('Error when sendEmail');
+        console.error(e);
+      }
+    }
+  });
