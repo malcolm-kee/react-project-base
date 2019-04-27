ec4ce26687b4f1b4d6e702adec9f7cd2dca90d37
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-04-27 10:48:16 +0800

    update firebase config

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,12 +1,12 @@
 import * as firebaseApp from 'firebase/app';
 
 const config = {
-  apiKey: 'AIzaSyAJXAgqJFE7ojoNBz6MrBvRidG-yyP39_g',
-  authDomain: 'smart-loan-9ec69.firebaseapp.com',
-  databaseURL: 'https://smart-loan-9ec69.firebaseio.com',
-  projectId: 'smart-loan-9ec69',
-  storageBucket: 'smart-loan-9ec69.appspot.com',
-  messagingSenderId: '347900906254'
+  apiKey: 'AIzaSyBbR6Ff0D0MzQcDvSfiPp4zuAIWgqKzTqc',
+  authDomain: 'smarty-loan.firebaseapp.com',
+  databaseURL: 'https://smarty-loan.firebaseio.com',
+  projectId: 'smarty-loan',
+  storageBucket: 'smarty-loan.appspot.com',
+  messagingSenderId: '900924052124'
 };
 
 firebaseApp.initializeApp(config);
